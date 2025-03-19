63a5678e8d5239debe3db7e98898f60f0ff68cd6
lizly15 <109866254+lizly15@users.noreply.github.com>
2025-03-20 00:33:02 +0700

    Using argument

--- main.cpp
+++ main.cpp
@@ -10,8 +10,74 @@
 #include "./include/shell_sort.h"
 #include "./include/sort_base.h"
 #include "./include/utils.h"
+#include "./include/data_generator.h"
 #include <iostream>
 #include <string>
+
+void choose_sort_algo(SortBase*& sort_algo, const string& algo) {
+    if(algo == "bubble-sort") {
+        sort_algo = new BubbleSort();
+        return;
+    }
+    if(algo == "counting-sort") {
+        sort_algo = new CountingSort();
+        return;
+    }
+    if(algo == "heap-sort") {
+        sort_algo = new HeapSort();
+        return;
+    }
+    if(algo == "insertion-sort") {
+        sort_algo = new InsertionSort();
+        return;
+    }
+    if(algo == "merge-sort") {
+        sort_algo = new MergeSort();
+        return;
+    }
+    if(algo == "quick-sort") {
+        sort_algo = new QuickSort();
+        return;
+    }
+    if(algo == "radix-sort") {
+        sort_algo = new RadixSort();
+        return;
+    }
+    if(algo == "selection-sort") {
+        sort_algo = new SelectionSort();
+        return;
+    }
+    if(algo == "shaker-sort") {
+        sort_algo = new ShakerSort();
+        return;
+    }
+    if(algo == "shell-sort") {
+        sort_algo = new ShellSort();
+        return;
+    }
+}
+
+void get_data(int*& a, int arr_n, string input_order, const string& file_name) {
+    if(input_order == "file") {
+        
+    }
+    else {
+        int order = 0;
+        if(input_order == "-rand") {
+            order = 0;
+        }
+        else if(input_order == "-sorted") {
+            order = 1;
+        }
+        else if(input_order == "-rev") {
+            order = 2;
+        }
+        else if(input_order == "-nsorted") {
+            order = 3;
+        }
+        GenerateData(a, arr_n, order);
+    }
+}
 
 int main(int argc, char* argv[])
 {
