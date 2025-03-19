a08a5582f080f55d24de4c0469de0f36568c9144
phamtienthanh21072003 <116580908+phamtienthanh21072003@users.noreply.github.com>
2025-03-19 15:33:34 +0700

    Update quick_sort.cpp

--- cpp/quick_sort.cpp
+++ cpp/quick_sort.cpp
@@ -1,10 +1,31 @@
 #include "../include/quick_sort.h"
 
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Choose the last element as the pivot
+    int i = (low - 1);     // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        // If current element is smaller than or equal to pivot
+        if (arr[j] <= pivot) {
+            i++; // increment index of smaller element
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
 void QuickSort::sort(int arr[], int n) {
     auto start = std::chrono::high_resolution_clock::now();
 
     // implement algorithm here
+    if (low < high) {
+        // pi is partitioning index, arr[p] is now at right place
+        int pi = partition(arr, low, high);
 
+        // Separately sort elements before partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
     auto end = std::chrono::high_resolution_clock::now();
     runtime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
