ce9e142ed837854243bb739f4d3ea368cd5d3fc9
phamtienthanh21072003 <116580908+phamtienthanh21072003@users.noreply.github.com>
2025-03-19 15:32:31 +0700

    Update heap_sort.cpp

--- cpp/heap_sort.cpp
+++ cpp/heap_sort.cpp
@@ -1,9 +1,47 @@
 #include "../include/heap_sort.h"
 
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize largest as root
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // If largest is not root
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+
 void HeapSort::sort(int arr[], int n) {
     auto start = std::chrono::high_resolution_clock::now();
 
     // implement algorithm here
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        std::swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
 
     auto end = std::chrono::high_resolution_clock::now();
     runtime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
